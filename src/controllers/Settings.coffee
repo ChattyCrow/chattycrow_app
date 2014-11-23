0c4b6df3b7b0ded0f50e3e711dadc9d55e0a6870
Strnadj <jan.strnadek@gmail.com>
2014-11-23 18:50:43 +0100

    Next

--- src/controllers/Settings.coffee
+++ src/controllers/Settings.coffee
@@ -4,8 +4,24 @@ class Settings
     @el = $('<div/>')
 
     that = this
-    @el.on 'click', '#saveSettings', ->
+    @el.on 'click', '#saveSettings', (evt) ->
+      evt.preventDefault()
       that.saveSettings()
+
+
+    @el.on 'click', '#scanQRCode', (evt) ->
+      evt.preventDefault()
+      cordova.plugins.barcodeScanner.scan(
+        (result) ->
+          if result.text.length > 0
+            $('#contactToken').val(result.text)
+            alert 'Success'
+          else
+            alert 'Error while scanning'
+        ,
+        (error) ->
+          alert "Error while scanning #{error}"
+      )
 
     # Return render
     @render
--- www/controllers/Settings.js
+++ www/controllers/Settings.js
@@ -6,8 +6,22 @@ Settings = (function() {
     this.service = service;
     this.el = $('<div/>');
     that = this;
-    this.el.on('click', '#saveSettings', function() {
+    this.el.on('click', '#saveSettings', function(evt) {
+      evt.preventDefault();
       return that.saveSettings();
+    });
+    this.el.on('click', '#scanQRCode', function(evt) {
+      evt.preventDefault();
+      return cordova.plugins.barcodeScanner.scan(function(result) {
+        if (result.text.length > 0) {
+          $('#contactToken').val(result.text);
+          return alert('Success');
+        } else {
+          return alert('Error while scanning');
+        }
+      }, function(error) {
+        return alert("Error while scanning " + error);
+      });
     });
     this.render;
   }
